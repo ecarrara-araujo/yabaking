f03cc14797014b5b65e00f9eb6c584b40acf1fce
Eduardo Carrara <ecarrara.araujo@gmail.com>
2017-10-03 10:12:41 -0300

    feature: adds media playing with ExoPlayer
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/main/java/br/com/ecarrara/yabaking/steps/presentation/navigating/StepDetailFragment.java
+++ app/src/main/java/br/com/ecarrara/yabaking/steps/presentation/navigating/StepDetailFragment.java
@@ -1,5 +1,7 @@
 package br.com.ecarrara.yabaking.steps.presentation.navigating;
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import br.com.ecarrara.yabaking.R;
 import br.com.ecarrara.yabaking.steps.domain.entity.Step;
@@ -30,6 +45,9 @@ public class StepDetailFragment extends Fragment {
         return stepDetailFragment;
     }
 
+    @BindView(R.id.step_detail_media_view)
+    SimpleExoPlayerView mediaPlayerView;
+
     @BindView(R.id.step_detail_short_description_text_view)
     TextView shortDescriptionTextView;
 
@@ -39,6 +57,8 @@ public class StepDetailFragment extends Fragment {
     private static final String LAST_KNOWN_STEP = "last_known_step";
 
     private Step step;
+    private SimpleExoPlayer mediaPlayer;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -72,11 +92,41 @@ public class StepDetailFragment extends Fragment {
         this.shortDescriptionTextView.setText(step.shortDescription());
         this.descriptionTextView.setText(step.description());
 
-        setUpMedia();
+        setUpMediaPlayer();
+        setUpMediaSource();
     }
 
-    private void setUpMedia() {
+    private void setUpMediaPlayer() {
+        mediaPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.color.accent));
 
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+        mediaPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+        mediaPlayerView.setPlayer(mediaPlayer);
+    }
+
+    private void setUpMediaSource() {
+        final String APPLICATION_BASE_USER_AGENT = "YaBaking";
+        final String userAgent = Util.getUserAgent(getContext(), APPLICATION_BASE_USER_AGENT);
+        MediaSource mediaSource = new ExtractorMediaSource(
+                Uri.parse(step.videoPath()),
+                new DefaultDataSourceFactory(getContext(), userAgent),
+                new DefaultExtractorsFactory(),
+                null, null);
+        mediaPlayer.prepare(mediaSource);
+        mediaPlayer.getPlayWhenReady();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
     @Override
