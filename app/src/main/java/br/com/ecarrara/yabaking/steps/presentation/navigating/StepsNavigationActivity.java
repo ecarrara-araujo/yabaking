6235843b313a6a3a84f140f938ecede56ad8b9bd
Eduardo Carrara <ecarrara.araujo@gmail.com>
2017-10-10 23:21:36 -0300

    fix: immersive fullscreen
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/main/java/br/com/ecarrara/yabaking/steps/presentation/navigating/StepsNavigationActivity.java
+++ app/src/main/java/br/com/ecarrara/yabaking/steps/presentation/navigating/StepsNavigationActivity.java
@@ -98,8 +98,12 @@ public class StepsNavigationActivity extends AppCompatActivity {
         if (isFullscreenLayout() && !isMultipaneLayout()) {
             View decorView = getWindow().getDecorView();
             int uiOptions = decorView.getSystemUiVisibility();
-            uiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            uiOptions ^= View.SYSTEM_UI_FLAG_LOW_PROFILE;
             uiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+            uiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            uiOptions ^= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            uiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            uiOptions ^= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(uiOptions);
         }
     }
